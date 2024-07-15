5ee29a3d3a76bc77f9298f3f7910ead4df44590a
Sam Neat <samjneat@gmail.com>
2024-07-15 15:11:42 +0100

    added more things to try

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    var activities = ["Archery", "Baseball", "Badminton", "Basketball", "Bowling", "Boxing", "Climbing", "Cricket", "Curling", "Fencing","Fishing", "Golf", "Handball","Hiking", "Hockey","Kickboxing","Lacrosse", "Pickleball", "Pilates", "Rugby", "Sailing", "Skateboarding", "Snowboarding","Surfing", "Softball", "Squash", "Taichi", "Tennis", "Volleyball", "Wrestling", "Yoga"]
     
     var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
     
